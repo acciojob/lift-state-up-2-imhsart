bc2cd44e0e989c0c745faa39117804b8bb8aff48
imhsart <nitinrana2519@gmail.com>
2026-04-15 14:44:41 +0530

    Assignment attempt 1

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,17 @@
 
 import React from "react";
 import './../styles/App.css';
+import { useState } from "react";
+import ChildComponent from "./ChildComponent";
+
 
 const App = () => {
+  const [inputValue, setInputValue] = useState('')
   return (
-    <div>
-        {/* Do not remove the main div */}
+    <div className="parent">
+        <h1>Parent Component</h1>
+        <div>{inputValue}</div>
+        <ChildComponent inputValue={inputValue} setInputValue={setInputValue} />
     </div>
   )
 }
---
+++ src/components/ChildComponent.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { useState } from 'react'
+
+const ChildComponent = ({inputValue, setInputValue}) => {
+
+  return (
+    <div className='child'>
+      <h2>Child Component</h2>
+      <input value={inputValue} onChange={(e) => setInputValue(e.target.value)}></input>
+    </div>
+  )
+}
+
+
+export default ChildComponent
